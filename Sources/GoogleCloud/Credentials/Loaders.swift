6b1326b6a0478043476fbc00497e3c42643a38a2
Anthony Castelli <neueanthony@me.com>
2018-10-02 20:00:33 -0700

    Use ENV Variables for App Engine

--- Sources/GoogleCloud/Credentials/Loaders.swift
+++ Sources/GoogleCloud/Credentials/Loaders.swift
@@ -10,6 +10,7 @@ import Foundation
 enum CredentialLoadError: Error {
     case fileLoadError
     case noValidFileError
+    case missingEnvironmentVariable
 }
 
 extension GoogleApplicationDefaultCredentials {
@@ -36,4 +37,19 @@ extension GoogleServiceAccountCredentials {
             throw CredentialLoadError.fileLoadError
         }
     }
+    
+    /*
+    init(type: String, projectId: String, privateKeyId: String, privateKey: String, clientEmail: String, clientId: String, authUri: URL, tokenUri: URL, authProviderX509CertUrl: URL, clientX509CertUrl: URL) {
+        self.type = type
+        self.projectId = projectId
+        self.privateKeyId = privateKeyId
+        self.privateKey = privateKey
+        self.clientEmail = clientEmail
+        self.clientId = clientId
+        self.authUri = authUri
+        self.tokenUri = tokenUri
+        self.authProviderX509CertUrl = authProviderX509CertUrl
+        self.clientX509CertUrl = clientX509CertUrl
+    }
+    */
 }
